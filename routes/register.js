be1ef01ab290809894376c3cda7259c913c60962
Diego Lorenesi <drlorenesi@gmail.com>
2021-01-26 18:35:49 -0600

    Add register with JWT.

--- routes/register.js
+++ routes/register.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const Joi = require('joi');
 const validate = require('../middleware/validate');
+const generateAuthToken = require('../utils/generateAuthToken');
 const db = require('../startup/db');
 
 const validateRegistration = (account) => {
@@ -27,12 +28,21 @@ router.post('/', [validate(validateRegistration)], async (req, res) => {
   let { firstName, lastName, email, password } = req.body;
   const salt = await bcrypt.genSalt(10);
   password = await bcrypt.hash(password, salt);
-  const result = await db.query(
+  const { rows } = await db.query(
     `INSERT INTO users (first_name, last_name, email, pass) 
       VALUES ($1, $2, $3, $4) RETURNING *`,
     [firstName, lastName, email, password]
   );
-  res.status(201).send(result.rows[0]);
+  // // Create payload and send JWT so user can directly log in
+  const payload = {
+    userId: rows[0].user_id,
+    roleId: rows[0].role_id,
+    firstName: rows[0].first_name,
+    lastName: rows[0].last_name,
+    email: rows[0].email,
+  };
+  const token = generateAuthToken(payload, process.env.jwtPrivateKey);
+  res.header('x-auth-token', token).send(payload);
 });
 
 module.exports = router;
