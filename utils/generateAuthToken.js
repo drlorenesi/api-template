1e197d72bfc9d178276ff54a3f825f3a8752a00a
Diego Lorenesi <drlorenesi@gmail.com>
2021-02-01 12:12:48 -0600

    Remove exp from JWT.

--- utils/generateAuthToken.js
+++ utils/generateAuthToken.js
@@ -2,6 +2,8 @@ const jwt = require('jsonwebtoken');
 
 // Creates a JWT that takes in a payload and key
 module.exports = (payload, key) => {
-  payload.exp = Math.floor(Date.now() / 1000) + 60 * 60;
+  // To add an expiration property:
+  // * 60 = 60 minutes
+  // payload.exp = Math.floor(Date.now() / 1000) + 60 * 60;
   return jwt.sign(payload, key);
 };
